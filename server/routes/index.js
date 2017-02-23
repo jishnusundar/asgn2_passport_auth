b833d3a4c13046b2e0017caaa9de72647fa31250
= <jishnusundar423@gmail.com>
2017-02-22 21:12:04 -0500

    Processed login and registration

--- server/routes/index.js
+++ server/routes/index.js
@@ -40,6 +40,13 @@ router.get('/login',(req, res, next) => {
   }
 });
 
+// POST /login - process the login
+router.post('/login', passport.authenticate('local', {
+successRedirect: '/contacts',
+failureRedirect: '/login',
+failureFlash: true
+}));
+
 /** ------------------------------- Routing for registration -------------------------------*/
 
 // GET /register - render the register page
@@ -56,33 +63,85 @@ if(!req.user) {
 }
 });
 
+// POST /register - process the registration page
+router.post('/register', (req, res, next) =>  {
+  User.register(
+    new User({
+        username: req.body.username,
+        password: req.body.password,
+        email: req.body.email,
+        displayName: req.body.displayName
+      }),
+      req.body.password,
+      (err) => {
+        if(err) {
+          console.log('Error insterting new user');
+          if(err.name == 'UserExistsError') {
+            req.flash('registerMessage', 'Registration Error: User Already Exists!');
+          }
+          return res.render('auth/register', {
+            title: 'Register',
+            //games: '',
+            messages: req.flash('registerMessage'),
+            displayName: req.user ? req.user.displayName : ''
+          });
+        }
+        // if registration is successful
+        return passport.authenticate('local')(req, res, ()=>{
+          res.redirect('/services');
+        });
+      });
+});
+
+/** ------------------------------- Routing for logout -------------------------------*/
+
+//GET /logout - Logout the user and redirect to the home page
+router.get('/logout', (req,res,next) => {
+req.logout();
+res.redirect('/'); //redirect to home page
+});
+
 /** ------------------------------- Routing for the 5 main pages -------------------------------*/
 
 /* GET projects page. */
 router.get('/projects', requireAuth,(req, res, next) => {
-  res.render('projects', { title: 'Projects'});
+  res.render('projects', { 
+    title: 'Projects',
+    displayName : req.user ? req.user.displayName : ''
+  });
 });
 
 /* GET services page. */
 router.get('/services', (req, res, next) => {
-  res.render('services', { title: 'Services' });
+  res.render('services', { 
+    title: 'Services' ,
+    displayName : req.user ? req.user.displayName : ''
+  });
 });
 
 /* GET about page. */
 router.get('/about', (req, res, next) => {
-  res.render('about', { title: 'Who am I?' });
+  res.render('about', { 
+    title: 'Who am I?' ,
+    displayName : req.user ? req.user.displayName : ''
+  });
 });
 
 /* GET contact page. */
 router.get('/contact', (req, res, next) => {
-  res.render('contact', { title: 'Reach Me' });
+  res.render('contact', { 
+    title: 'Reach Me' ,
+    displayName : req.user ? req.user.displayName : ''
+  });
 });
 
 /* GET home page. */
 router.get('/', (req, res, next) => {
   let currentDate = new Date();
   res.render('home', { 
-    title: 'Hi there!'  , mainContent: 'My name is Jishnu and i\'ve been studying Software Engineering Technology for the last 2 years @ Centennial College. This website is my simple portfolio where most of my accomplishments and interests are posted. Check in to see!'
+    title: 'Hi there!'  , 
+    mainContent: 'My name is Jishnu and i\'ve been studying Software Engineering Technology for the last 2 years @ Centennial College. This website is my simple portfolio where most of my accomplishments and interests are posted. Check in to see!' ,
+    displayName : req.user ? req.user.displayName : ''
   });
 });
 
