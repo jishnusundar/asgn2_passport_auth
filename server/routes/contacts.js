75d752b2ba29d7dccdd2cdb15d11a11eeec9dd87
= <jishnusundar423@gmail.com>
2017-02-23 14:28:19 -0500

    Added full CRUD functionality

--- server/routes/contacts.js
+++ server/routes/contacts.js
@@ -39,4 +39,102 @@ router.get('/',(req, res, next) => {
 
 });
 
+//  GET the Contact Details page in order to add a new Contact
+router.get('/add', (req, res, next) => {
+  res.render('contacts/details', {
+    title: "Add a new Contact",
+    contacts:'',
+  displayName : req.user ? req.user.displayName : ''
+  });
+});
+
+// POST process the Contact Details page and create a new Contact - CREATE
+router.post('/add', (req, res, next) => {
+
+    let newContact = contact({
+      "name": req.body.name,
+      "number": req.body.number,
+      "email": req.body.email
+    });
+
+    contact.create(newContact, (err, contact) => {
+      if(err) {
+        console.log(err);
+        res.end(err);
+      } else {
+        res.redirect('/contacts');
+      }
+    });
+});
+
+
+// GET the Contact Details page in order to edit a Contact
+router.get('/:id',(req, res, next) => {
+
+    try {
+      // get a reference to the id from the url
+      let id = mongoose.Types.ObjectId.createFromHexString(req.params.id);
+
+        // find one contact by its id
+      contact.findById(id, (err, contacts) => {
+        if(err) {
+          console.log(err);
+          res.end(error);
+        } else {
+          // show the contact details view
+          res.render('contacts/details', {
+              title: 'Contact Details',
+              contacts: contacts,
+  displayName : req.user ? req.user.displayName : ''
+          });
+        }
+      });
+
+    } catch (err) {
+      console.log(err);
+      res.redirect('/errors/404');
+    }
+});
+
+// POST - process the information passed from the details form and update the document
+router.post('/:id',(req, res, next) => {
+  // get a reference to the id from the url
+    let id = req.params.id;
+
+     let updatedContact = contact({
+       "_id": id,
+      "name": req.body.name,
+      "number": req.body.number,
+      "email": req.body.email
+    });
+
+    contact.update({_id: id}, updatedContact, (err) => {
+      if(err) {
+        console.log(err);
+        res.end(err);
+      } else {
+        // refresh the Contacts List
+        res.redirect('/contacts');
+      }
+    });
+
+});
+
+// GET - process the delete by Contact id
+router.get('/delete/:id',(req, res, next) => {
+  // get a reference to the id from the url
+    let id = req.params.id;
+
+    contact.remove({_id: id}, (err) => {
+      if(err) {
+        console.log(err);
+        res.end(err);
+      } else {
+        // refresh the contacts list
+        res.redirect('/contacts');
+      }
+    });
+});
+
+
 module.exports = router;
